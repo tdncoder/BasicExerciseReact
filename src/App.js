c302a23b6c3fb10e6fa1a35e994f43ba6c365d60
tdncoder <92639411+tdncoder@users.noreply.github.com>
2024-03-26 13:04:12 +0700

    Basic Exercise React

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const message = ["Learn React", "Think like God in React", "Get more income"];
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => step + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {message[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7590f9", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7590f9", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
